9ae6a9ce9063f583233d8e48a6b9ae3d85fd998c
DavidGalileo24 <david.salgado.mejia@gmail.com>
2024-03-10 23:15:49 -0600

    Change typography and change button components

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
 import Nav from '@/Components/Nav.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
 
 defineProps({});
 </script>
@@ -8,6 +10,11 @@ defineProps({});
 <template>
     <Head title="David Salgado | Desarrollador web" />
     <Nav />
+    <div class="w-full text-white">
+        <h1 class="text-5xl text-center">David Salgado</h1>
+        <SecondaryButton>Descargar CV</SecondaryButton>
+        <PrimaryButton>Descargar CV</PrimaryButton>
+    </div>
 </template>
 
 <style scoped></style>
